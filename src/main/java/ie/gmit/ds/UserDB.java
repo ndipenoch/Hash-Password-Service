919a5e7827d93ff8b6cc6e6cff97f56cbedb17a4
Mark Ndipenoch <G00352031@GMIT.IE>
2019-11-22 17:45:47 +0000

    HashMap user database created.

--- src/main/java/ie/gmit/ds/UserDB.java
+++ src/main/java/ie/gmit/ds/UserDB.java
@@ -1,4 +1,19 @@
 package ie.gmit.ds;
 
+import com.google.protobuf.ByteString;
+import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class UserDB {
+
+    //Users database
+    public final static HashMap<Integer, User> users = new HashMap<>();
+    static{
+        //Test data
+        users.put(1, new User(1, "Mark", "mark@gmit.ie"));
+        users.put(2, new User(2, "James",  "james@gmail.com"));
+    }
 }
