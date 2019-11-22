a641c9c50344db107308bc811050926daafd5110
Mark Ndipenoch <G00352031@GMIT.IE>
2019-11-22 18:10:05 +0000

    Validator method added in the Apiresource class.

--- src/main/java/ie/gmit/ds/UserApiResource.java
+++ src/main/java/ie/gmit/ds/UserApiResource.java
@@ -1,4 +1,28 @@
 package ie.gmit.ds;
 
+// Adapted from https://howtodoinjava.com/dropwizard/tutorial-and-hello-world-example/
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/users")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 public class UserApiResource {
+
+    private final Validator validator;
+
+    public UserApiResource(Validator validator) {
+        this.validator = validator;
+    }
+    
 }
